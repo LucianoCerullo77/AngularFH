4fa2f15c2a49776454101068bdb0774b35f485ab
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-16 10:23:40 -0300

    basic types of ts

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,7 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
+import './topic/01-basic-types.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hello World
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ 01-typescript-intro/src/topic/01-basic-types.ts
@@ -0,0 +1,13 @@
+const name: string = "Sean";
+
+let hpPoints: number | "full" = 123;
+
+const isAlive : boolean = true
+
+hpPoints = "full"
+
+console.log({name,hpPoints,isAlive});
+
+// hpPoints= true
+
+export {};
