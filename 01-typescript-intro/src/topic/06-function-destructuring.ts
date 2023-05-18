298a9af9d850cff01e4b382434a56fb54a1e8acd
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-17 22:35:20 -0300

    arguments destructuring

--- 01-typescript-intro/src/topic/06-function-destructuring.ts
+++ 01-typescript-intro/src/topic/06-function-destructuring.ts
@@ -1,2 +1,38 @@
+interface Product {
+  description: string;
+  price: number;
+}
 
-export {}
+const phone: Product = {
+  description: "Nokia A1",
+  price: 150,
+};
+
+const tablet: Product = {
+  description: "iPad Air",
+  price: 200,
+};
+
+interface TaxCalculationOptions {
+  tax: number;
+  products: Product[];
+}
+
+function taxCalculation(options: TaxCalculationOptions): number[] {
+  let total = 0;
+  options.products.forEach((product) => (total += product.price));
+  return [total, total * options.tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const result = taxCalculation({
+  products: shoppingCart,
+  tax,
+});
+
+console.log("Total", result[0]);
+console.log("Tax:", result[1]);
+
+export {};
