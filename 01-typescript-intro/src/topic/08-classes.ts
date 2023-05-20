5847d6552fd2714328fbbbae23c1b5b5d8401b7c
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-20 19:41:33 -0300

    class constructor

--- 01-typescript-intro/src/topic/08-classes.ts
+++ 01-typescript-intro/src/topic/08-classes.ts
@@ -1,18 +1,21 @@
 export class Person {
-  public name: string;
-  private address: string;
+  //   public name: string;
+  //   private address: string;
+
   // if the property has the ? means that could be string || undefined
   // if the declaration is private it means it can't be visible on the site and we need to use it in our code
   // if the declaration is public, means it can be visible
 
-  constructor() {
-    this.name = "Pedro";
-    this.address = "New York";
+  constructor(public name: string, private address: string) {
+    // is more common to declare the type on the constructor
+    // dependency injection on Angular !!!!
+    this.name = name;
+    this.address = address;
   }
 }
 
-const ironMan = new Person();
+const ironMan = new Person("Tony Stark", "Florida");
 
 // ironman.address shouldn't be visible on the console, but typescript compiles to js, so it can be visible, but in ts
 // is gonna show an error
-console.log(ironMan.name);
+console.log(ironMan);
