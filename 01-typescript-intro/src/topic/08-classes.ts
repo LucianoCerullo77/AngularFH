2dee83b943a401c570d539ec8491102aeaca570c
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-20 20:09:34 -0300

    dependency injection with herence composition

--- 01-typescript-intro/src/topic/08-classes.ts
+++ 01-typescript-intro/src/topic/08-classes.ts
@@ -6,25 +6,42 @@ export class Person {
   // if the declaration is private it means it can't be visible on the site and we need to use it in our code
   // if the declaration is public, means it can be visible
 
-  constructor(public name: string, private address: string) {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    private address: string = "No address"
+  ) {
     // is more common to declare the type on the constructor
     // dependency injection on Angular !!!!
-    this.name = name;
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.address = address;
   }
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string
+//   ) {
+//     super(realName,'New York');
+//   }
+// }
+
+export class Hero {
   constructor(
     public alterEgo: string,
     public age: number,
-    public realName: string
+    public realName: string,
+    public person: Person
   ) {
-    super(realName,'New York');
+    // this.person = new Person(realName);
   }
 }
 
-const ironMan = new Hero("IronMan", 45, "Tony Stark");
+const tony = new Person("Tony", "Stark", "New York");
+const ironMan = new Hero("IronMan", 45, "Tony Stark", tony);
 
 // ironman.address shouldn't be visible on the console, but typescript compiles to js, so it can be visible, but in ts
 // is gonna show an error
