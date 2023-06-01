dd34e717ea619420108b98511b7d79622f5373cd
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-06-01 11:54:48 -0300

    ngFor aplicated with heroNames

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-
+  public heroNames: string[] = [
+    'Spider-Man',
+    'Iron-Man',
+    'Ant-Man',
+    'Hulk',
+    'Thor',
+  ];
 }
