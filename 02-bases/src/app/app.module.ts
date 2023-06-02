fcbd50dbf9b77b505ef945ab9c55ed2f3bdfebdb
desarrollolc <lucianonicolas10@gmail.com>
2023-06-02 16:49:20 -0300

    module imported in app module

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [AppComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 02-bases/src/app/counter/counter.module.ts
+++ 02-bases/src/app/counter/counter.module.ts
@@ -2,5 +2,6 @@ import { NgModule } from '@angular/core';
 import { CounterComponent } from './components/counter/counter.component';
 @NgModule({
   declarations: [CounterComponent],
+  exports: [CounterComponent],
 })
 export class CounterModule {}
