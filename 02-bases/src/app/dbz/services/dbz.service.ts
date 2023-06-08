e0b7af23efe2b512e2dd87650ef688d70e897ab8
desarrollolc <lucianonicolas10@gmail.com>
2023-06-08 12:44:19 -0300

    uuid implemented on characters

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -6,18 +6,19 @@ import { Character } from '../interfaces/character.interface';
 @Injectable({
   providedIn: 'root',
 })
-
 export class DbzService {
   public characters: Character[] = [
     {
       name: 'Krilin',
       power: 5000,
+      id: uuid(),
     },
     {
       name: 'Goku',
       power: 20000,
+      id: uuid(),
     },
-    { name: 'Vegeta', power: 18000 },
+    { name: 'Vegeta', power: 18000, id: uuid() },
   ];
 
   onNewCharacter(character: Character): void {
