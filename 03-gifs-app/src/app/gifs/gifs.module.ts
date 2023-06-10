c504f99a32eac83fdbfecb8b48f56c25269b77b7
desarrollolc <lucianonicolas10@gmail.com>
2023-06-10 12:02:33 -0300

    card list component created and exported

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
   imports: [CommonModule],
-  exports: [HomePageComponent, SearchBoxComponent],
+  exports: [HomePageComponent, SearchBoxComponent, CardListComponent],
 })
 export class GifsModule {}
