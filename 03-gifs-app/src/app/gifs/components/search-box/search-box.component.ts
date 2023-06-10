88cd77d91b7f17aebd11da865b541c5dcf3dd404
desarrollolc <lucianonicolas10@gmail.com>
2023-06-10 12:23:56 -0300

    local reference with value implemented

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-search-box',
   template: `<h5>Search</h5>
-  <input type="text" class="form-control" placeholder="Search Gifs . . .">`,
-  styleUrls: ['./search-box.component.css']
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Search Gifs . . ."
+      (keyup)="searchTag(txtTagInput.value)"
+      #txtTagInput
+    />`,
+  styleUrls: ['./search-box.component.css'],
 })
 export class SearchBoxComponent {
-
+  searchTag(newTag: string): void {
+    console.log({ newTag });
+  }
 }
