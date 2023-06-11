66f25a68c60f951e634da161beabdc420d8b6579
desarrollolc <lucianonicolas10@gmail.com>
2023-06-11 13:22:36 -0300

    search ang get tags methods created at gifs service

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class GifsService {
+  private _tagsHistory: string[] = [];
 
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
 
+  searchTag(tag: string):void {
+    this._tagsHistory.unshift(tag);
+  }
 }
