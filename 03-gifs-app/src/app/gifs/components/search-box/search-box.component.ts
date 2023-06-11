689b53c5bcb396d63010bc2e6fc132292730faf3
desarrollolc <lucianonicolas10@gmail.com>
2023-06-11 13:26:15 -0300

    services injected in search box component

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -16,8 +17,13 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({ newTag });
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
   }
 }
