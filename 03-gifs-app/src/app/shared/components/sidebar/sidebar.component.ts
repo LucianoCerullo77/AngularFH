0748c204d0c94c3523f0c05b6f345827fbe3826a
desarrollolc <lucianonicolas10@gmail.com>
2023-06-13 16:09:13 -0300

    get gifs based on history applied

--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-
 import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-
   constructor(private gifsService: GifsService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  get tags(): string[] {
+    return this.gifsService.tagsHistory;
   }
 
-  get tags() {
-    return this.gifsService.tagsHistory
-
+  showGifByHistory(tag: string): void {
+    this.gifsService.searchTag(tag);
   }
 }
