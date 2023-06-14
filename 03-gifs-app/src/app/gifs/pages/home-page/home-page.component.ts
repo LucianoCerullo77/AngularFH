fb4bb220e96a74008ce0065ad17fb2c71cc80fc5
desarrollolc <lucianonicolas10@gmail.com>
2023-06-14 20:53:23 -0300

    updating home page css

--- 03-gifs-app/src/app/gifs/pages/home-page/home-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
+  styleUrls:['./home-page.component.css']
 })
 export class HomePageComponent {
   constructor(private gifsService: GifsService) {}
