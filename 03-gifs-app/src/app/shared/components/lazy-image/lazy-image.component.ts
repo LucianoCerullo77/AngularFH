c4c59b93cf44f169b88dddeceb597770667384d6
desarrollolc <lucianonicolas10@gmail.com>
2023-06-16 13:25:55 -0300

    loader implemented on cards

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
-  styleUrls: ['./lazy-image.component.css'],
 })
-export class LazyImageComponent {}
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is Required!');
+  }
+}
