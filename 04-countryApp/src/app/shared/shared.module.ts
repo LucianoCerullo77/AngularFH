f1662106ed0960bb89c050e8fa36b67c75444764
desarrollolc <lucianonicolas10@gmail.com>
2023-06-19 11:41:40 -0300

    sidebar component created and imported

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
   imports: [CommonModule],
-  exports: [HomePageComponent, AboutPageComponent],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
 })
 export class SharedModule {}
