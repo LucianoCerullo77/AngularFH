28eb2457f8ebeee088786fc5037c2bab408e2ea3
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-06-26 21:47:34 -0300

    search Capital service implemented with interfaces applied

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesServices {
-  constructor(private httpClient: HttpClient) {}
+  private apiURL: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) {}
+  searchCapital(term: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`);
+  }
 }
