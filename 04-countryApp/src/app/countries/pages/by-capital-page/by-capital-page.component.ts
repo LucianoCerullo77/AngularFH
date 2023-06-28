52374a88ff0d6960fb08eedcccd5ce4d2b491044
desarrollolc <lucianonicolas10@gmail.com>
2023-06-27 21:17:37 -0300

    showing data from countries on the capital table

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CountriesServices } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countriesServices: CountriesServices) {}
+
   searchByCapital(term: string) {
-    console.log({ term });
+    this.countriesServices.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
 }
