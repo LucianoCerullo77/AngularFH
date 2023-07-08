3309a613e78d374b9d7ce8fdd2a63b3c1b614bf5
desarrollolc <lucianonicolas10@gmail.com>
2023-07-08 14:02:28 -0300

    loader showing while searching

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -8,12 +8,16 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCapitalPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countriesServices: CountriesServices) {}
 
   searchByCapital(term: string) {
+    this.isLoading = true;
+
     this.countriesServices.searchCapital(term).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
