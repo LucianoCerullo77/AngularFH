e2e280741af3399bf21d60cf8755cc71cbdd5b6a
desarrollolc <lucianonicolas10@gmail.com>
2023-07-11 20:13:18 -0300

    debouncer applied

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,16 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  private debouncer: Subject<string> = new Subject<string>();
+
   @Input()
   public placeholder: string = '';
 
   @Output() public onValue: EventEmitter<string> = new EventEmitter();
 
+  ngOnInit(): void {
+    this.debouncer.subscribe((value) => {
+      console.log('debouncer value', value);
+    });
+  }
+
   emitValue(value: string): void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
   }
 }
