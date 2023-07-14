7876c276e8c19d69d09af8256af5e312156c2f8b
desarrollolc <lucianonicolas10@gmail.com>
2023-07-14 18:26:18 -0300

    regions type updated

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesServices } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,8 +11,14 @@ import { CountriesServices } from '../../services/countries.service';
 export class ByRegionPageComponent {
   constructor(private countriesServices: CountriesServices) {}
 
-  @Input()
   public countries: Country[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
 
   searchByRegion(term: string) {
     this.countriesServices
