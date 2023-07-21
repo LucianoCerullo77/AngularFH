35d4cf4a60b4fabebebb092e7196864c6da62d71
desarrollolc <lucianonicolas10@gmail.com>
2023-07-21 20:49:38 -0300

    new component and module created

---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+
+}
