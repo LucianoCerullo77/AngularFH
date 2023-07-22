7d28964fedec8fbab55807b1c92ad837a5ed3886
desarrollolc <lucianonicolas10@gmail.com>
2023-07-21 21:15:07 -0300

    menu applied for primeng module

--- 05-pipesApp/src/app/shared/components/menu/menu.component.ts
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MenuComponent {
+  public menuItems: MenuItem[] = [];
 
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'New',
+        icon: 'pi pi-fw pi-plu',
+      },
+      {
+        label: 'Open',
+        icon: 'pi pi-fw pi-download',
+      },
+      {
+        label: 'Undo',
+        icon: 'pi pi-fw pi-refresh',
+      },
+    ];
+  }
 }
