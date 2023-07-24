c65e052e181200a3dd74036dd70c820ac7221a0f
desarrollolc <lucianonicolas10@gmail.com>
2023-07-24 16:46:46 -0300

    new components created at pages folder

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
