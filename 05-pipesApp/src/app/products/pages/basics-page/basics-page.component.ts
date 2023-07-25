85c8f1719be7f0edefeefba2ccb98dd1bca04c07
desarrollolc <lucianonicolas10@gmail.com>
2023-07-25 20:39:34 -0300

    adding pipes with titles on basics page component

--- 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'products-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'angular';
+  public nameUpper: string = 'ANGULAR';
+  public fullname: string = 'AnGuLaR FrAmEwOrK';
 }
