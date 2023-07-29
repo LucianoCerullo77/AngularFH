88938d17fe9ed47f3feb0a9f55e703b69d5052ee
desarrollolc <lucianonicolas10@gmail.com>
2023-07-29 20:34:55 -0300

    date pipes applied to basic page component

--- 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
@@ -9,4 +9,6 @@ export class BasicsPageComponent {
   public nameLower: string = 'angular';
   public nameUpper: string = 'ANGULAR';
   public fullname: string = 'AnGuLaR FrAmEwOrK';
+
+  public customDate: Date = new Date();
 }
