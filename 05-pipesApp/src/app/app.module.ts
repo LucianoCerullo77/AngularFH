fd3a0a79f0c1b1024431c4bfab8b6959b3679c23
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-08-03 17:09:28 -0300

    global locale language applied with US and CA lenguages

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { SharedModule } from './shared/shared.module';
 
 // locale config of the app
 
 import localeEsAR from '@angular/common/locales/es-AR';
+import localeEnUS from '@angular/common/locales/en-NA';
+import localeFrCA from '@angular/common/locales/fr-CA';
 import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localeEsAR);
+registerLocaleData(localeEnUS);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +24,12 @@ registerLocaleData(localeEsAR);
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-AR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
