5f23fed1d74fe9c4b157831371ed529737cd935b
desarrollolc <lucianonicolas10@gmail.com>
2023-08-09 11:08:24 -0300

    basic html shows on i18n Select section

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
-
+  // i18n Select
+  public name: string = 'John Doe';
+  public gender: 'male' | 'female' = 'male';
 }
