77019804f545dc072c526d1eda6c9f0e1ca7e5db
desarrollolc <lucianonicolas10@gmail.com>
2023-08-16 20:09:29 -0300

    pipe personalized

--- 05-pipesApp/src/app/products/pipes/toggle-case.pipe.ts
+++ 05-pipesApp/src/app/products/pipes/toggle-case.pipe.ts
@@ -6,10 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'toggleCase',
 })
-
-
 export class ToggleCasePipe implements PipeTransform {
-  transform(value: string): string {
-    return value.toUpperCase();
+  transform(value: string, toUpper: boolean = false): string {
+    return toUpper ? value.toUpperCase() : value.toLowerCase();
   }
 }
