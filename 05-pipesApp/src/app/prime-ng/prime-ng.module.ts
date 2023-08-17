e2a8c182fe0fc004825f59f2870f92b52e48aea2
desarrollolc <lucianonicolas10@gmail.com>
2023-08-17 13:24:35 -0300

    toolbar implemented

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -5,6 +5,7 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
@@ -14,6 +15,7 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     PanelModule,
+    ToolbarModule,
   ],
 })
 export class PrimeNgModule {}
