77f448a24ce9ab01f533e354ac7ef85208b0c8cd
desarrollolc <lucianonicolas10@gmail.com>
2023-08-24 11:44:09 -0300

    sortBy pipe updated and applied to html

--- 05-pipesApp/src/app/products/pipes/sort-by.pipe.ts
+++ 05-pipesApp/src/app/products/pipes/sort-by.pipe.ts
@@ -4,20 +4,29 @@ import { Hero } from '../interfaces/hero.interface';
 @Pipe({
   name: 'sortBy',
 })
+// export class SortByPipe implements PipeTransform {
+//   transform(heroes: Hero[], sortBy?: keyof Hero | ''): Hero[] {
+//     switch (sortBy) {
+//       case 'name':
+//         return heroes.sort((a, b) => (a.name > b.name )? 1 : -1);
+
+//       case 'canFly':
+//         return heroes.sort((a, b) => (a.name > b.name )? 1 : -1);
+
+//       case 'color':
+//         return heroes.sort((a, b) => (a.name > b.name )? 1 : -1);
+
+//       default:
+//         return heroes;
+//     }
+//   }
+// }
+
 export class SortByPipe implements PipeTransform {
-  transform(heroes: Hero[], sortBy: keyof Hero | null): Hero[] {
-    switch (sortBy) {
-      case 'name':
-        return heroes.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-      case 'canFly':
-        return heroes.sort((a, b) => (a.name > b.name ? 1 : -1));
+  transform(heroes: Hero[], sortBy: keyof  Hero | '' = ''): Hero[] {
 
-      case 'color':
-        return heroes.sort((a, b) => (a.name > b.name ? 1 : -1));
-
-      default:
-        return heroes;
-    }
+    return sortBy !== '' ? heroes.sort((a,b)=> a[sortBy] > b[sortBy] ? 1 : -1) : heroes;
   }
+
 }
