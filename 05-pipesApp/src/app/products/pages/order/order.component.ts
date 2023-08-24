a03a87f6f62475c758ab53dcdc753e0a4dde3cbf
desarrollolc <lucianonicolas10@gmail.com>
2023-08-24 11:55:05 -0300

    pipes app updated with buttons applied to order pipes, pipes completed

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -8,6 +8,8 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero;
+
   public heroes: Hero[] = [
     {
       name: 'SuperMan',
@@ -48,5 +50,9 @@ export class OrderComponent {
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero) {
+    this.orderBy = value;
   }
 }
