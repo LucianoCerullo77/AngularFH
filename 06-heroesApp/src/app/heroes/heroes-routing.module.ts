bf1234e8f4cd94588bbc8d377997108be501e962
desarrollolc <lucianonicolas10@gmail.com>
2023-08-26 09:27:35 -0300

    layout pages implemented to auth and heroes main routes

--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
