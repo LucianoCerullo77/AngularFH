71b0efc4a75e618e2a825ff7462bb23018f62457
desarrollolc <lucianonicolas10@gmail.com>
2023-08-26 09:36:12 -0300

    layout children created at heroes routing

--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent,
+      },
+    ],
   },
 ];
 
