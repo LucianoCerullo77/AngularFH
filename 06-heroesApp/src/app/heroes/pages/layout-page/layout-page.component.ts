1b315e532513907ca938180db4d52572576efe04
desarrollolc <lucianonicolas10@gmail.com>
2023-09-02 23:07:55 -0300

    sidenav link routed to current pages, icons added

--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    { label: 'List', icon: 'label', url: './list' },
+    { label: 'Add', icon: 'add', url: './new-hero' },
+    { label: 'Search', icon: 'search', url: './search' },
+  ];
 }
