e21c6751f12ce3d01d93c76525dbf597ff9e7c05
desarrollolc <lucianonicolas10@gmail.com>
2023-09-02 22:32:52 -0300

    material modules imported to hero modules

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
