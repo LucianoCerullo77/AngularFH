3e3f10e88b781cf722a42a380065ac0685c8eba1
desarrollolc <lucianonicolas10@gmail.com>
2023-09-13 20:11:25 -0300

    hero page component updated with hero service methods

--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private heroService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        return;
+      });
+    throw new Error('Method not implemented.');
+  }
 }
