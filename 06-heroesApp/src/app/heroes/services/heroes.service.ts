cda9e27bc608c839ae6032bbd397ce9f28d6951a
desarrollolc <lucianonicolas10@gmail.com>
2023-09-13 19:44:50 -0300

    new service method created, getHeroById done

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from 'src/environments/environments';
 
@@ -12,5 +12,11 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseURL}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 }
