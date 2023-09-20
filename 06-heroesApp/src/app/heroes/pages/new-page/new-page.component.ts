e774402bf4616bccc4ff9d7a8d8ad4fd575f9257
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-09-20 20:22:30 -0300

    getter current hero created as new method on component

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -31,10 +32,17 @@ export class NewPageComponent {
     },
   ];
 
+  constructor(private heroesService: HeroesService) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+  }
+
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value,
-    });
+    if (this.heroForm.invalid) return;
+
+    // this.heroesService.updateHero(this.heroForm.value);
   }
 }
