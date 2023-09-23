9b643880d0be7612d210cd705569586a178ef3b4
desarrollolc <lucianonicolas10@gmail.com>
2023-09-22 21:16:25 -0300

    adding methods to ngOnInit function to reset form

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics',
+    },
+  ];
+
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('', { nonNullable: true }),
@@ -21,23 +34,28 @@ export class NewPageComponent {
     alt_img: new FormControl(''),
   });
 
-  public publishers = [
-    {
-      id: 'DC Comics',
-      desc: 'DC - Comics',
-    },
-    {
-      id: 'Marvel Comics',
-      desc: 'Marvel - Comics',
-    },
-  ];
-
-  constructor(private heroesService: HeroesService) {}
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
 
     return hero;
+  }
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigateByUrl('/');
+        this.heroForm.reset(hero);
+        return;
+      });
   }
 
   onSubmit(): void {
@@ -50,8 +68,8 @@ export class NewPageComponent {
       return;
     }
 
-    this.heroesService.addHero(this.currentHero).subscribe(hero => {
+    this.heroesService.addHero(this.currentHero).subscribe((hero) => {
       // show snackbar and redirect to /heroes/edit/ hero.id
-    })
+    });
   }
 }
