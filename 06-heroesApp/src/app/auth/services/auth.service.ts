a59ec9fdf7172aa42eb96e0852d81cae645a2328
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-10-01 20:09:29 -0300

    getter of current user done, User interface created

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
-export class AuthService {
-  constructor(private http: HttpClient) { }
+import { User } from '../interfaces/user.interface';
+import { environment } from '../../../environments/environments';
 
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseURL = environment.baseURL;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return this.user;
+  }
 }
