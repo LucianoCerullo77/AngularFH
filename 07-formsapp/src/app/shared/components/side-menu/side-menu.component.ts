d30680c4088c056517d414cd7e5d6fc0311d1d26
desarrollolc <lucianonicolas10@gmail.com>
2023-10-13 22:21:36 -0300

    sidebar menu routes implemented

--- 07-formsapp/src/app/shared/components/side-menu/side-menu.component.ts
+++ 07-formsapp/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  title: string;
+  route: string;
+}
 @Component({
   selector: 'shared-side-menu',
   templateUrl: './side-menu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SideMenuComponent {
+  public reactiveMenu: MenuItem[] = [
+    { title: 'Basicos', route: './reactive/basic' },
+    { title: 'Dinámicos', route: './reactive/dynamic' },
+    { title: 'Switches', route: './reactive/switches' },
+  ];
 
+  public authMenu: MenuItem[] = [{ title: 'Registro', route: './auth' }];
 }
