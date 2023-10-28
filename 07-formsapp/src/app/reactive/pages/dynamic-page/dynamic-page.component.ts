37742d3d6879f25f1281478b80a032f61b0f62fc
desarrollolc <lucianonicolas10@gmail.com>
2023-10-27 22:01:46 -0300

    creating dynamic page component.ts

--- 07-formsapp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsapp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
-
+  public ProductForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
+  constructor(private fb: FormBuilder) {}
 }
