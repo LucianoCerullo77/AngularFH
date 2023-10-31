3d39ce8f6b8a2056c5cc45c711358fd1d5169629
desarrollolc <lucianonicolas10@gmail.com>
2023-10-31 20:14:16 -0300

    updating onSubmit method

--- 07-formsapp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsapp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent {
-  public ProductForm: FormGroup = this.fb.group({
+  public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     favouriteGames: this.fb.array([
       ['Metal Gear', Validators.required],
@@ -13,4 +13,14 @@ export class DynamicPageComponent {
     ]),
   });
   constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
