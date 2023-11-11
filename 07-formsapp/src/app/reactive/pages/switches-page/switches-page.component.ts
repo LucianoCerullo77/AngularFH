6a97087ffb4b2f52f72e4e5c4668dac13e9a8038
desarrollolc <lucianonicolas10@gmail.com>
2023-11-11 16:52:27 -0300

    updating reactive form with data save implemented

--- 07-formsapp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsapp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,29 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [],
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     gender: ['M', Validators.required],
     wantNotifications: [true, Validators.required],
     termsAndConditions: [false, Validators.requiredTrue],
   });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
   constructor(private fb: FormBuilder) {}
 
-  onSave() {
-    if (this.myForm.invalid) {
-      this.myForm.markAllAsTouched;
-      return;
-    }
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
   }
 
   isValidField(field: string): boolean | null {
     return (
       this.myForm.controls[field].errors && this.myForm.controls[field].touched
     );
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched;
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+
+    console.log(this.myForm.value);
+    console.log(this.person);
   }
 }
