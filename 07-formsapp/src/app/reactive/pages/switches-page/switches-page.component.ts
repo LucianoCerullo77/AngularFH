442e30129cf80704297a52185812c75592e33af9
desarrollolc <lucianonicolas10@gmail.com>
2023-11-10 21:22:45 -0300

    starting switches page, with form validators on work

--- 07-formsapp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsapp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched;
+      return;
+    }
+  }
 }
