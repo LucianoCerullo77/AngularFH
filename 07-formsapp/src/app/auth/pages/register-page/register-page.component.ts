126637f5fbf208ef3139cab5588912b84d2eff2e
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-11-13 11:55:28 -0300

    registerpage component started with formGroup values and onSubmit function created

--- 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(6)],
+    confirmPassword: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: getting this via service.
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
