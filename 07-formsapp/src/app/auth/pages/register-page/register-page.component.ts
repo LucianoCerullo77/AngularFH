41e02b153531b242c242d1c3eb2bab1879f2cc31
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-11-23 09:38:08 -0300

    validator functions created and connected to username, it cant be strider !

--- 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -7,11 +8,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    username: ['', Validators.required],
-    password: ['', Validators.required, Validators.minLength(6)],
-    confirmPassword: ['', Validators.required],
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required], [Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) {}
