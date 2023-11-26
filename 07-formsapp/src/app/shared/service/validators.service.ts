33a5a4f8d78e5e3d8f27cedf1e6384ab7d2a531d
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-11-26 20:51:09 -0300

    solving error with typo on validators.service

--- 07-formsapp/src/app/shared/service/validators.service.ts
+++ 07-formsapp/src/app/shared/service/validators.service.ts
@@ -3,12 +3,11 @@ import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
 export class ValidatorsService {
-
   public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   public emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   cantBeStrider = (control: FormControl): ValidationErrors | null => {
-    const value: string = control.value.trim().toLowercase();
+    const value: string = control.value.trim().toLowerCase();
 
     if (value === 'strider') {
       return {
