27dde907f2c9d7234b16771e44793c676170496c
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-11-25 21:33:50 -0300

    validators service completed and implemented on register page html and ts files

--- 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsapp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from 'src/app/shared/validators/validators';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
+// import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -11,21 +12,27 @@ export class RegisterPageComponent {
     name: [
       '',
       [Validators.required],
-      Validators.pattern(customValidators.firstNameAndLastnamePattern),
+      Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
     ],
     email: [
       '',
-      [Validators.required, Validators.pattern(customValidators.emailPattern)],
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.emailPattern),
+      ],
     ],
-    username: ['', [Validators.required, customValidators.cantBeStrider]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required], [Validators.minLength(6)]],
     confirmPassword: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
 
   isValidField(field: string) {
-    // TODO: getting this via service.
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
--- 07-formsapp/src/app/shared/service/validators.service.ts
+++ 07-formsapp/src/app/shared/service/validators.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
-export class ValidatorsService {}
+export class ValidatorsService {
+
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+  cantBeStrider = (control: FormControl): ValidationErrors | null => {
+    const value: string = control.value.trim().toLowercase();
+
+    if (value === 'strider') {
+      return {
+        noStrider: true,
+      };
+    }
+
+    return null;
+  };
+
+  public isValidField(form: FormGroup, field: string): boolean | null {
+    return form.controls[field].errors && form.controls[field].touched;
+  }
+}
