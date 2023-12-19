8a25d4b443f33ff3434d2de2242b1826c5389466
desarrollolc <lucianonicolas10@gmail.com>
2023-12-19 08:02:16 -0300

    email validator updated with suscriber methods

--- 07-formsapp/src/app/shared/validators/email-validator.service.ts
+++ 07-formsapp/src/app/shared/validators/email-validator.service.ts
@@ -8,13 +8,36 @@ import { Observable, delay, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidatorService implements AsyncValidator {
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(delay(2000));
+  // }
+
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
 
-    console.log({ email });
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
 
-    return of({
-      emailTaken: true,
-    }).pipe(delay(2000));
+        if (email === 'prueba@mail.com') {
+          subscriber.next({ emailTaken: true });
+          subscriber.complete();
+          // return;
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(
+      delay(3000)
+    )
+
+    return httpCallObservable;
   }
 }
