d32dfd785df6450c7a4fc7c7bc13ba95b6219ebc
desarrollolc <lucianonicolas10@gmail.com>
2024-01-07 23:15:20 -0300

    baseUrl created

--- 08-selectors/src/app/countries/services/countries.service.ts
+++ 08-selectors/src/app/countries/services/countries.service.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { Region, SmallCountry } from '../interfaces/country.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
 
-  private _regions: Region[] = [Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania];
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania,
+  ];
 
-  constructor() { }
+  constructor() {}
 
   get regions(): Region[] {
     return [...this._regions];
   }
 
   getCountriesByRegion(region: Region): SmallCountry[] {
-    return []
+    return [];
   }
-
 }
